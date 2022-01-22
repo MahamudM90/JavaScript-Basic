a91ede96a535ad9879d99aee8236f5ac0cc22588
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-01-22 16:54:26 +0600

    program

---
+++ secondprogram.js
@@ -0,0 +1,8 @@
+
+<html>
+<body>
+<h2>What Can JavaScript Do?</h2>
+<p id="demo">JavaScript Can Hide Html Content</p>
+<button type="button" onclick="document.getElementById('demo').style.display='none'">click</button>
+</body>
+</html>
