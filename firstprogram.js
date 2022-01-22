2529973554296825573615e59d41abaf01d2f80c
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-01-22 16:33:39 +0600

    first commit

---
+++ firstprogram.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>What Can JavaScript Do?</h2>
+<p id="demo">JavaScript Can Change Html Content</p>
+<button type="button" onclick='document.getElementById("demo").innerHTML="hello"'>click</button>
+</body>
+</html>
