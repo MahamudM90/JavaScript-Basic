1a63adb24df1839392aa197def3c74ab6f55f376
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-02-03 10:29:02 +0600

    boolean

---
+++ boolean.js
@@ -0,0 +1,16 @@
+var firstName = 'Mahamud';
+var age = 22;
+
+if (age < 13) {
+  // if age is less than 13 then this condition will execute
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 22) {
+  // if age is greater than or equal to 13 and age is less than 20 then this condition will execute
+  console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+  // if age is greater than or equal to 20 and age is less than 30 then this condition will execute
+  console.log(firstName + ' is a young man.');
+} else {
+  // if above condition's false then else statment execuete
+  console.log(firstName + ' is a man.');
+}
