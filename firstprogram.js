9d3155166b5a8ca0dd24b597801acf220ef16587
Md. Mahamud Hasan <73417906+MahamudM90@users.noreply.github.com>
2022-05-17 11:30:19 +0600

    Update firstprogram.js

--- firstprogram.js
+++ firstprogram.js
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h2>What Can JavaScript Do?</h2>
+<h2>What Can JavaScript do?</h2>
 <p id="demo">JavaScript Can Change Html Content</p>
 <button type="button" onclick='document.getElementById("demo").innerHTML="hello"'>click</button>
 </body>
