360792049cb28fd22c16df4bdd65c89104e78af3
Md. Mahamud Hasan <73417906+MahamudM90@users.noreply.github.com>
2022-05-25 15:29:09 +0600

    Update secondprogram.js

--- secondprogram.js
+++ secondprogram.js
@@ -2,7 +2,7 @@
 <html>
 <body>
 <h2>What Can JavaScript To Do?</h2>
-<p id="demo">JavaScript Can Hide Html Content</p>
+<p id="demo">JavaScript Can Hide Html COntent</p>
 <button type="button" onclick="document.getElementById('demo').style.display='none'">Click</button>
 </body>
 </html>
